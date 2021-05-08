74058415732c4844bdfbd0cd4bf87c5b9222c19a
Ji <ji01jeong@gmail.com>
2021-05-08 00:18:58 -0700

    Added two activities along with their respective .xml files (Home Screen after clicking "log in" and Sign Up page) and functioning "Remember me" checkbox and "Sign Up" text leads to Sign Up page/activity.

--- app/src/main/java/com/example/overflow/MainActivity.java
+++ app/src/main/java/com/example/overflow/MainActivity.java
@@ -1,14 +1,46 @@
 package com.example.overflow;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.content.Intent;
+import android.widget.CheckBox;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    public Button button;
+    private CheckBox RembrMe;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Login Button
+        button = (Button) findViewById(R.id.LoginBtn);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent (MainActivity.this, HomeScreen.class);
+                startActivity(intent);
+            }
+        });
+
+        //RembrMe Checkbox
+        RembrMe = findViewById(R.id.RememberMe);
+
+        //Forgot Password
+
+        //Sign Up
+        final TextView signUp = this.findViewById(R.id.SignUp);
+        signUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent (MainActivity.this, SignUp.class);
+                startActivity(intent);
+            }
+        });
     }
 }
