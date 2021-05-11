3887755d4d1da3391f6444bdb273e9b06ddc7e89
Adriana Lee <adriylee@gmail.com>
2021-05-11 01:54:50 -0700

    sign up name boxes, cart items, sign up pop up (toast), sign up move to home page

--- app/src/main/java/com/example/overflow/SignUp.java
+++ app/src/main/java/com/example/overflow/SignUp.java
@@ -2,19 +2,43 @@ package com.example.overflow;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.Spinner;
 import android.widget.Toast;
 
 public class SignUp extends AppCompatActivity {
+    public Button button;
+    private CheckBox RembrMe;
+
+    public void displayToast(View v){
+        Toast.makeText(SignUp.this,"Registration Successful",Toast.LENGTH_SHORT).show();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        //Successful Registry Button
+
+
+        //Sign up Button
+        button = (Button) findViewById(R.id.signup);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent (SignUp.this, HomeScreen.class);
+                startActivity(intent);
+            }
+        });
+
 
         //Account Type Drop-down
         Spinner mySpinner = (Spinner) findViewById(R.id.accountType);
@@ -23,4 +47,5 @@ public class SignUp extends AppCompatActivity {
         myAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         mySpinner.setAdapter(myAdapter);
     }
+
 }
